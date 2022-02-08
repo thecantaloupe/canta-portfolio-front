376d5cf805a6e177e617b53f5edf510bd1015448
Zachary Gomez <zgomez301@gmail.com>
2022-02-07 19:58:54 -0800

    update

---
+++ src/styles/Style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { Link } from "react-router-dom";
+
+
+
+export const NavStyle = styled.nav`
+    display: flex;
+    justify-content: space-around;
+    border: 3px solid black;
+    padding: 8px;
+    width: 90%;
+    margin: auto;
+`
+export const NavLink = styled(Link) `
+
+`
